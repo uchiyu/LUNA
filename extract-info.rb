a0b26be6f54f306000aa1d088a1a2155c5f0ed87
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2016-01-29 14:35:53 +0900

    add writing csv file

--- extract-info.rb
+++ extract-info.rb
@@ -1,47 +1,82 @@
 #config: utf-8
 
 require "rexml/document"
+require "csv"
 
 def extract_info( directory, file )
 
   %x( unzip #{directory}#{file} ppt/slides/slide1.xml )
 
-  text = ""
-  doc = REXML::Document.new(File.new("ppt/slides/slide1.xml"))
+  text = ''
+  doc = REXML::Document.new(File.new('ppt/slides/slide1.xml'))
   doc = doc.to_s.scan(/<p:txBody>(.*?)<\/p:txBody>/)
-  doc.each{|doc_elem|
+  doc.each do |doc_elem|
     arr = doc_elem.to_s.scan(/<a:t>(.*?)<\/a:t>/)
-    arr.each{|str_elem|
+    arr.each do |str_elem|
       text << str_elem.to_s
-    }
-    text << ","
-  }
-  %x(rm -rf _rels ppt)
-  return text.delete("\"").delete("[").delete("]")
-end
-
-def trim_text( text )
-  arr = text.to_s.split(",")
-  arr.each{|text_block|
-    # 情報のマッチング
-    puts text_block.match(/\d{2}[T|G|t|g]\d{3}/)
-    puts text_block.match(/月例発表([ ]|)\d{4}年\d{2}月版/)
-    puts text_block.match(/^(?!.*(月例発表([ ]|)\d{4}年\d{2}月版|\d{2}[T|G|t|g]\d{3}|富永研究室)).+$/)
-    # puts text_block.match(/\d{2}[T|G|t|g]\d{3}[ ](.*)[ |　|\d{2}[T|G|t|g]\d{3}]/).to_s # 名前の抽出
-  }
-end
-
-def find_file()
-  Dir.glob('./**/*').each do |file|
-
-    if /^(?!(.*)\/s\d{2}[T|G|t|g]\d{3}\/\d{6}\/(.*)\/(.*)).*.pptx/ =~ file
-      puts file
     end
+    text << ','
+  end
+  %x(rm -rf _rels ppt)
+  return text.delete('\"').delete('[').delete(']')
+end
+
+def trim_text( text, file )
+
+  write = Array.new(4)
+  arr = text.to_s.split(',') # テキストの結合
+
+  write[0] = file
+
+  arr.each do |text_block|
+    # 情報のマッチング
+    tmp = text_block.match(/\d{2}[T|G|t|g]\d{3}/).to_s.strip
+    if tmp != ''
+      write[1] = tmp
+    end
+
+    tmp = text_block.match(/\d{4}年\d{2}月版/).to_s.strip
+    if tmp != ''
+      write[2] = tmp
+    end
+
+    tmp = text_block.match(/^(?!.*(月例発表([ ]|)\d{4}年\d{2}月版|\d{2}[T|G|t|g]\d{3}|富永研究室)).+$/).to_s.strip
+    if tmp != ''
+      write[3] = tmp
+    end
+  end 
+  
+  path = 'sample.csv'
+  CSV.open(path, "a") do |csv|
+    csv << write
   end
 end
 
-find_file()
-#string = extract("resume/semi2013/s13t210/201503/", "s13t210-201503.pptx")
-#puts string
-#puts
-#trim( string )
+def find_file()
+  paths = Array.new
+  Dir.glob('./**/*').each do |path|
+
+    # /年月/*.pptx の場合のみ
+    if /^(?!(.*)\/s\d{2}[T|G|t|g]\d{3}\/\d{6}\/(.*)\/(.*)).*.pptx/ =~ path
+      paths.push(path)
+    end
+  end
+  return paths
+end
+
+def divide_filename( paths, files, directories )
+  paths.each do |path|
+    #puts path
+    files.push(path.to_s.slice!(/[^\/]*$/))
+    directories.push(path.delete(path.to_s.slice!(/[^\/]*$/)))
+  end
+end
+
+paths = find_file()
+#files = Array.new
+#directories = Array.new
+#divide_filename(paths, files, directories)
+#files.zip(directories).each do |file, directory| # zip で配列の合成
+#  string = extract_info(directory, file)
+#  trim_text( string, file )
+#end
