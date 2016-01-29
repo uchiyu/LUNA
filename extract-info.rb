5126d71a5b0668bbd7d7d740da4ee0a12d3f53f8
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2016-01-29 09:22:51 +0900

    extract slide title

--- extract-info.rb
+++ extract-info.rb
@@ -8,20 +8,30 @@ def extract( directory, file )
 
   text = ""
   doc = REXML::Document.new(File.new("ppt/slides/slide1.xml"))
-  arr = doc.to_s.scan(/<a:t>(.*?)<\/a:t>/)
-  arr.each{|str_elem|
-    text << str_elem.to_s
+  doc = doc.to_s.scan(/<p:txBody>(.*?)<\/p:txBody>/)
+  doc.each{|doc_elem|
+    arr = doc_elem.to_s.scan(/<a:t>(.*?)<\/a:t>/)
+    arr.each{|str_elem|
+      text << str_elem.to_s
+    }
+    text << ","
   }
-
   %x(rm -rf _rels ppt)
   return text.delete("\"").delete("[").delete("]")
 end
 
 def trim( text )
-  puts text.match(/\d{2}[T|G|t|g]\d{3}/)
-  puts text.match(/月例発表\d{4}年\d{2}月版/)
-  puts text.match(/\d{2}[T|G|t|g]\d{3}[ ](.*?)香川大学/).to_s.gsub(/\d{2}[T|G|t|g]\d{3}[ ]/, "").delete("香川大学")
+  arr = text.to_s.split(",")
+  arr.each{|text_block|
+    # 情報のマッチング
+    puts text_block.match(/\d{2}[T|G|t|g]\d{3}/)
+    puts text_block.match(/月例発表([ ]|)\d{4}年\d{2}月版/)
+    puts text_block.match(/^(?!.*(月例発表([ ]|)\d{4}年\d{2}月版|\d{2}[T|G|t|g]\d{3}|富永研究室)).+$/)
+    # puts text_block.match(/\d{2}[T|G|t|g]\d{3}[ ](.*)[ |　|\d{2}[T|G|t|g]\d{3}]/).to_s # 名前の抽出
+  }
 end
 
-string = extract("resume/semi2013/s13t208/201503/", "s13t208-201503.pptx")
+string = extract("resume/semi2013/s13t210/201503/", "s13t210-201503.pptx")
+puts string
+puts
 trim( string )
