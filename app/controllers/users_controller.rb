e280cd6e5c12b4767e48e2875e41dfd3da0aa4be
ucchi_in <s13t208@stmail.eng.kagawa-u.ac.jp>
2016-05-26 22:32:21 +0900

    ユーザー登録の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,18 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def destroy
+  end
+
+  def create
+    @user = User.new
+    @user.student_id = params[:user][:student_id]
+    @user.name = params[:user][:name]
+    @user.password_digest = params[:user][:password]
+    @user.save
+    redirect_to '/users/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'users/destroy'
 
+  post "users" => "users#create"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
