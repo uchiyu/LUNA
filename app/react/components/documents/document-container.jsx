10350ac6350bcfec51d1ed5da87612c395487cb3
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2016-12-31 23:07:44 +0900

    add category form

--- app/react/components/documents/document-container.jsx
+++ app/react/components/documents/document-container.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import DocumentTable from './document-table.jsx';
 import KeywordForm from './keyword-form.jsx';
+import CategoryForm from './category-form.jsx';
 
 class DocumentContainer extends React.Component {
   render() {
     return(
       <div>
         <KeywordForm filter = {this.props.filter} />
+        <CategoryForm filter = {this.props.filter} />
         <DocumentTable documentData = {this.props.documentData} />
       </div>
     );
