886daa0a1236db0c62e35f90199cefc94257a9da
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2017-03-11 05:48:40 +0900

    document table sort by date

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,6 @@
 class DocumentsController < ApplicationController
   def index
-    documentData = Document.all.map { |doc| { student_num: doc.student_num, title: doc.title, doc_num: doc.doc_num, category: doc.category, visible: true } }
+    documentData = Document.all.order(doc_num: :desc).map { |doc| { student_num: doc.student_num, title: doc.title, doc_num: doc.doc_num, category: doc.category, visible: true } }
     searchKey = { student_num: '', title_queries: [], doc_num: '', category_queries: [] }
     @documents = { documentData: documentData, searchKey: searchKey }
   end
